a77bbf6499ef8f6cd4b5527cd79564b2700219e8
Dave Ankin <daveankin@gmail.com>
2023-03-04 03:22:09 -0500

    add button to go to somewhere

--- app/src/main/java/smsviewer/app/MainActivity.kt
+++ app/src/main/java/smsviewer/app/MainActivity.kt
@@ -1,11 +1,18 @@
 package smsviewer.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import smsviewer.app.crud_example.ExamplesActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun navigateToExamples(view: View) {
+        startActivity(Intent(this, ExamplesActivity::class.java));
     }
 }
