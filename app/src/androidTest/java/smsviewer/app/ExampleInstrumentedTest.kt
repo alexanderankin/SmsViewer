f546c220e885d99f7518d8c0b10dab1ad3a0c0a1
Dave Ankin <daveankin@gmail.com>
2023-03-04 04:35:15 -0500

    fix test - packageName = applicationId

--- app/src/androidTest/java/smsviewer/app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/smsviewer/app/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("smsviewer.app", appContext.packageName)
+        assertEquals("info.ankin.smsviewer", appContext.packageName)
     }
 }
